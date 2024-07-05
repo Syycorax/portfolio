fbe8dc4c72c21cf0f41192c029c04e6c269663f6
Syycorax <54723548+Syycorax@users.noreply.github.com>
2024-07-05 20:51:18 +0200

    first version deployed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import githubLogo from '/github.svg'
-import avatar from '/avatar.png'
-import syswatchimg from '/syswatch.png'
-import goforkimg from '/gofork.png'
-import tipeImg from '/tipe.png'
-import wave from '/wave.png'
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import avatar from './assets/avatar.png'
+import wave from './assets/wave.png'
+import syswatchimg from './assets/syswatch.png'
+import goforkimg from './assets/gofork.png'
+import tipeImg from './assets/tipe.png'
+import githubLogo from './assets/github.svg'
+import issimg from './assets/isstracker.png'
+import tildeimg from './assets/tilde.png'
+import codeplaygroundimg from './assets/codeplayground.png'
 import './App.css'
 
 function App() {
@@ -16,20 +18,35 @@ function App() {
         <img src={avatar} className="logo" alt="My avatar" />
       </div>
       <h1>Hi <img src={wave} className="emoji" alt="👋" /> <br></br>
-        I'm Hector Nussbaumer</h1>
+        I'm Hector</h1>
       <h2>IT and network engineer in apprenticeship student based in Paris, France</h2>
-      <div className="card">
-        <button>
-          My resume
-        </button>
-        <button>
+      <div className="buttonContainer">
+        <button onClick={() => document.querySelector(".mansorycontainer")?.scrollIntoView({ behavior: 'smooth' })}>
           Discover my work !
         </button>
+        <button onClick={() => window.location.href = "mailto:cv@syycorax.xyz?subject=Contact Request: Resume&body=(please customize this)%0AHi,%0AI would like to know more about you, can I see your resume"}>
+          My resume
+        </button>
+        <button onClick={() => window.location.href = "https://github.com/Syycorax"}>
+          My Github
+        </button>
+        <button onClick={() => window.location.href = "mailto:contact@syycorax.xyz"}>
+          Contact me
+        </button>
       </div >
-      <div className="showcaseFlex">
-        <Project name="Syswatch" description="My personal system monitoring tool" image={syswatchimg} link="https://github.com/Syycorax/syswatch" />
-        <Project name="Gofork" description="Gofork is a CLI tool to find forks that are ahead of a github repository." image={goforkimg} link="https://github.com/Syycorax/gofork" />
-        <Project name="TIPE" description="Simulation of a blackjack game and implementation of a decision tree" image={tipeImg} link="https://github.com/Syycorax/tipe" />
+      <div className="mansorycontainer">
+        <ResponsiveMasonry
+          columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
+        >
+          <Masonry gutter="16px">
+            <Project name="Syswatch" description="My personal system monitoring tool" image={syswatchimg} link="https://github.com/Syycorax/syswatch" />
+            <Project name="Gofork" description="Gofork is a CLI tool to find forks that are ahead of a github repository." image={goforkimg} link="https://github.com/Syycorax/gofork" />
+            <Project name="TIPE" description="Simulation of a blackjack game and implementation of a decision tree" image={tipeImg} link="https://github.com/Syycorax/tipe" />
+            <Project name="isstracker" description="A twitter bot that tweets the current position of the ISS" image={issimg} link="https://github.com/Syycorax/isstracker" />
+            <Project name="tilde" description="A fork of the tilde project, my current homepage" image={tildeimg} link="https://github.com/Syycorx/tilde" />
+            <Project name="CodePlayground" description="A vscode extension that allows you create a temporary file and run it" image={codeplaygroundimg} link="https://github.com/Syycorax/codeplayground" />
+          </Masonry>
+        </ResponsiveMasonry>
       </div>
     </>
   )
@@ -48,7 +65,9 @@ function Project(props: ProjectProps) {
   const { name, description, image, link } = props;
   return (
     <div className="projectcard">
-      <img src={image} alt={name} className='projectimg' />
+      <div className="projectImgContainer">
+        <img src={image} alt={name} className='projectimg' />
+      </div>
       <div className="cardFlex">
         <h3 className='projectname'>{name}</h3>
         <a href={link} className='projectlink'>
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Helmet } from 'react-helmet'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Helmet>
+      <title>Hector's Portfilio</title>
+      <meta name="title" content="Hector's Portfilio" />
+      <meta name="description" content="Hector's collection of projects " />
+      <meta name="keywords" content="portfolio,hector,projects,github" />
+      <meta name="robots" content="index, follow" />
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="language" content="English" />
+
+    </Helmet>
     <App />
   </React.StrictMode>,
 )
